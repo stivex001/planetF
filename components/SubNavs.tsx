f0d5fe773db9023451d7c7f9435756bc9d7ea228
stivex001 <stephenadeyemo@gmail.com>
2023-11-18 15:28:13 +0100

    feat:implemented the subnavebar components

--- components/SubNavs.tsx
+++ components/SubNavs.tsx
@@ -23,7 +23,7 @@ const navbarLinks = [
     sublinks: [
       { id: 1, title: "Fund Wallet", url: "/user/fundwallet" },
       { id: 2, title: "Bill Payment", url: "/user/billpayment" },
-      { id: 3, title: "CG Wallet", url: "/user/cdwallet" },
+      { id: 3, title: "CD Wallet", url: "/user/cdwallet" },
       { id: 4, title: "Sales Report", url: "/user/salesreport" },
     ],
   },
@@ -36,77 +36,121 @@ const navbarLinks = [
   },
   {
     id: 3,
-    icon: <FiActivity size={24} className="-ml-3 xl:ml-0"/>,
+    icon: <FiActivity size={24} className="-ml-3 xl:ml-0" />,
     title: "Commission List",
     url: "/user/commission",
     arrowIcon: <IoIosArrowDown className="hidden xl:block" />,
   },
   {
     id: 4,
-    icon: <FiLayout size={24} className="-ml-3 xl:ml-0"/>,
+    icon: <FiLayout size={24} className="-ml-3 xl:ml-0" />,
     title: "Promocode",
     url: "/user/promocode",
     arrowIcon: <IoIosArrowDown className="hidden xl:block" />,
   },
   {
     id: 5,
-    icon: <BsInbox size={24} className="-ml-3 xl:ml-0"/>,
+    icon: <BsInbox size={24} className="-ml-3 xl:ml-0" />,
     title: "Upgrade",
     url: "/user/upgrade",
     arrowIcon: <IoIosArrowDown className="hidden xl:block" />,
   },
   {
     id: 6,
-    icon: <FiSidebar size={24} className="-ml-3 xl:ml-0"/>,
+    icon: <FiSidebar size={24} className="-ml-3 xl:ml-0" />,
     title: "Refer and Earn",
     url: "/user/refer",
     arrowIcon: <IoIosArrowDown className="hidden xl:block" />,
   },
   {
     id: 7,
-    icon: <FiHardDrive size={24} className="-ml-3 xl:ml-0"/>,
+    icon: <FiHardDrive size={24} className="-ml-3 xl:ml-0" />,
     title: "Settings",
     url: "/user/settings",
-    arrowIcon: <IoIosArrowDown className="hidden xl:block"  />,
+    arrowIcon: <IoIosArrowDown className="hidden xl:block" />,
   },
 ];
 
 type Props = {};
 
 const SubNavs = (props: Props) => {
-  const [activeLink, setActiveLink] = useState("Dashboard");
+  const [activeLink, setActiveLink] = useState<string | null>(null);
 
-  const handleLinkClick = (linkId: any) => {
-    setActiveLink(linkId);
+  // const handleLinkClick = (linkId: any) => {
+  //   setActiveLink(linkId);
+  // };
+
+  const handleLinkClick = (linkId: string) => {
+    setActiveLink((prevActiveLink) =>
+      prevActiveLink === linkId ? null : linkId
+    );
   };
 
   return (
     <nav className="w-full ">
       <ul className="flex flex-col justify-between gap-5 whitespace-nowrap w-full">
         {navbarLinks.map((link) => (
-          <Link
-            href={`${link.url}`}
-            key={link.id}
-            className={` hover:bg-slate-100 transition duration-200 p-4 rounded-xl w-[20%] xl:w-full group ${
-              activeLink === link.title ? "bg-slate-100 " : ""
-            } `}
-            onClick={() => handleLinkClick(link.title)}
-          >
-            <div
-              className={`flex items-center justify-between gap-2 group-hover:text-[#164e63] ${
-                activeLink === link.title ? "text-[#164e63]" : ""
-              }`}
-            >
-              <div className="flex items-center gap-2 ">
-                {link.icon}
-                <span className="hidden xl:flex text-sm font-medium text-[#164e63]">
-                  {link.title}
-                </span>
+          <li key={link.id}>
+            {link.sublinks ? (
+              <div
+                className={`hover:bg-slate-100 transition duration-200 p-4 rounded-xl w-[20%] xl:w-full group ${
+                  activeLink === link.title ? "bg-slate-100 " : ""
+                } `}
+                onClick={() => handleLinkClick(link.title)}
+              >
+                <div className="flex items-center justify-between gap-2 group-hover:text-[#164e63] cursor-pointer">
+                  <div className="flex items-center gap-2">
+                    {link.icon}
+                    <span className="hidden xl:flex text-sm font-medium text-[#164e63]">
+                      {link.title}
+                    </span>
+                  </div>
+                  {link.sublinks && (
+                    <IoIosArrowDown
+                      className={`${
+                        activeLink === link.title ? "transform rotate-180" : ""
+                      } hidden xl:block`}
+                    />
+                  )}
+                </div>
               </div>
+            ) : (
+              <Link href={`${link.url}`}>
+                <div
+                  className={`hover:bg-slate-100 transition duration-200 p-4 rounded-xl w-[20%] xl:w-full group ${
+                    activeLink === link.title ? "bg-slate-100 " : ""
+                  } `}
+                  onClick={() => handleLinkClick(link.title)}
+                >
+                  <div className="flex items-center justify-between gap-2 group-hover:text-[#164e63]">
+                    <div className="flex items-center gap-2">
+                      {link.icon}
+                      <span className="hidden xl:flex text-sm font-medium text-[#164e63]">
+                        {link.title}
+                      </span>
+                    </div>
+                    {link.arrowIcon}
+                  </div>
+                </div>
+              </Link>
+            )}
 
-              {link.arrowIcon}
-            </div>
-          </Link>
+            {/* Render sublinks if they exist */}
+            {link.sublinks && activeLink === link.title && (
+              <ul className=" mt-2 p-2 bg-slate-100 border border-gray-200 rounded shadow flex flex-col gap-4  ">
+                {link.sublinks.map((sublink) => (
+                  <li key={sublink.id}>
+                    <Link href={`${sublink.url}`} className="flex items-center  justify-center xl:justify-start gap-2">
+                      <FiActivity size={24} className="-ml-3 xl:ml-0" />
+                      <span className="hidden xl:flex text-sm font-medium text-gray-800 hover:text-[#164e63]">
+                        {sublink.title}
+                      </span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
         ))}
       </ul>
     </nav>
