f9d7d9c6dea74ccf6ba1d1e8ae8646e2efae1ffb
stivex001 <stephenadeyemo@gmail.com>
2023-11-23 04:36:23 +0100

    feat:integrating nextAuth package in the project

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QjXlL8DDjbGygPmy7Du9hWdFB8ZeAn0Ph/Aj+CURhpw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"v/OU7mbDyaYKhhmQY+B1vl/THPo7r2YO4/bQvfuKXUQ=\"\n}"
